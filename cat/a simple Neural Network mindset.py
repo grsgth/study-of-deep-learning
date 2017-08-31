ed5e11d9e487e64025ab6d4be25dc74b51cc4d1b
grsgth <625477614@qq.com>
2017-08-31 14:54:45 +0800

    Add files via upload

---
+++ cat/a simple Neural Network mindset.py
@@ -0,0 +1,94 @@
+import numpy as np
+
+def sigmoid(z):
+    s = 1/(1+np.exp(-z))
+    return s
+
+def initialize_with_zeros(dim):
+    w = np.zeros((dim,1))
+    b = 0
+    return w,b
+
+def propagate(w, b, X, Y):
+    A = sigmoid(np.dot(w.T,X)+b)
+    m = X.shape[1]
+    
+    cost = (Y*np.log(A)+(1-Y)*np.log(1-A)).sum()/-m
+    
+    dw = np.dot(X,(A-Y).T)/m
+    db = (A-Y).sum()/m
+    grads = {"dw":dw,"db":db}
+    
+    return grads,cost
+
+def optimize(w, b, X, Y, num_iterations, learning_rate, print_cost = False):
+    for i in range(num_iterations):
+        costs = []
+        grads,cost = propagate(w, b, X, Y)
+        dw = grads["dw"]
+        db = grads["db"]
+        w = w - learning_rate*dw
+        b = b - learning_rate*db
+        
+        if i % 100 == 0:
+            costs.append(cost)
+        if print_cost and i % 100 ==0:
+            print("Cost after iteration %i:%f" %(i, cost))
+    
+    params = {"w": w, "b": b}
+    grads = {"dw": dw,"db": db}
+    
+    return params,grads,costs
+
+def predict(w, b, X):
+    m = X.shape[1]
+    Y_prediction = np.zeros((1,m))
+    w = w.reshape(X.shape[0],1)
+    
+    A = sigmoid(np.dot(w.T,X)+b)
+    
+    for i in range(A.shape[1]):
+        if A[0,i]<=0.5:
+            Y_prediction[0,i] = 0
+        else:
+            Y_prediction[0,i] = 1
+    
+    return Y_prediction
+
+def model(X_train, Y_train, X_test, Y_test, num_iterations = 2000, learning_rate = 0.5, print_cost = False):
+    w,b = initialize_with_zeros(X_train.shape[0])
+    parameters, grads, costs = optimize(w, b, X_train, Y_train, num_iterations, learning_rate, print_cost)
+    w = parameters["w"]
+    b = parameters["b"]
+    
+    Y_prediction_train = predict(w, b, X_train)
+    Y_prediction_test = predict(w, b, X_text)
+    
+    print("train accuracy: {} %".format(100 - np.mean(np.abs(Y_prediction_train - Y_train)) * 100))
+    print("test accuracy: {} %".format(100 - np.mean(np.abs(Y_prediction_test - Y_test)) * 100))
+    
+    d = {"costs": costs,
+         "Y_prediction_test": Y_prediction_test, 
+         "Y_prediction_train" : Y_prediction_train, 
+         "w" : w, 
+         "b" : b,
+         "learning_rate" : learning_rate,
+         "num_iterations": num_iterations}
+    
+    return d
+
+#test
+w, b, X, Y = np.array([[1],[2]]), 2, np.array([[1,2],[3,4]]), np.array([[1,0]])
+grads, cost = propagate(w, b, X, Y)
+print ("dw = " + str(grads["dw"]))
+print ("db = " + str(grads["db"]))
+print ("cost = " + str(cost))
+
+params, grads, costs = optimize(w, b, X, Y, num_iterations= 100, learning_rate = 0.009, print_cost = False)
+
+print ("w = " + str(params["w"]))
+print ("b = " + str(params["b"]))
+print ("dw = " + str(grads["dw"]))
+print ("db = " + str(grads["db"]))
+
+print ("predictions = " + str(predict(w, b, X)))
